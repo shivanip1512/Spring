d112d217ff96d5e61a9c967f665c55d93e3e3f50
shivanip1512 <shivanipacharne10@gmail.com>
2021-11-29 23:42:47 +0530

    inserted record

--- SpringJdbcTemplate/src/main/java/com/java/spring/jdbcTemplate/App.java
+++ SpringJdbcTemplate/src/main/java/com/java/spring/jdbcTemplate/App.java
@@ -1,13 +1,21 @@
 package com.java.spring.jdbcTemplate;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/java/spring/jdbcTemplate/config.xml");
+		JdbcTemplate jdbcTemplate = context.getBean("jdbcTemplate",JdbcTemplate.class);
+		
+		//insert query
+		String iq = "insert into student (id,name, address) values (?,?,?)";
+		int res = jdbcTemplate.update(iq,03,"Kirti","pune");
+		System.out.println("number of record(s) inserted :"+res);
+	}
 }
