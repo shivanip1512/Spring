ffbaf5d62eaca0a5873c8f96759e3c1c5731a7f9
shivanip1512 <shivanipacharne10@gmail.com>
2021-12-04 17:04:26 +0530

    send data from controller to view

--- springMVC/src/main/java/springMVC/controller/HomeController.java
+++ springMVC/src/main/java/springMVC/controller/HomeController.java
@@ -1,13 +1,15 @@
 package springMVC.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("name", "Shivani");
 		return "index";
 	}
 	
--- springMVC/src/main/webapp/WEB-INF/views/index.jsp
+++ springMVC/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
 <h2>Welcome to home Page!</h2>
+<%
+String name = (String) request.getAttribute("name");
+%>
+<h1>How are you <%=name%>? </h1>
 </body>
 </html>
