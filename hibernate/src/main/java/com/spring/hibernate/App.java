f869be662c0e66ebc3fa4d2927fecea95a3b3164
shivanip1512 <shivanipacharne10@gmail.com>
2021-12-19 19:17:40 +0530

    added hibernate properties

--- hibernate/src/main/java/com/spring/hibernate/App.java
+++ hibernate/src/main/java/com/spring/hibernate/App.java
@@ -1,13 +1,18 @@
 package com.spring.hibernate;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("hi");
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory(); // //
+		System.out.println(factory);
+	}
 }
